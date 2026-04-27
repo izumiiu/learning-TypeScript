ec7be7d00a3471741c1cb8e04035cc746155370a
izumiiu <peerawat.ez@gmail.com>
2026-04-27 23:56:16 +0700

    firstcommit (variable)

---
+++ dist/app.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let tName = "Tjay test";
+let age = 30;
+let isWorking = false;
+age += 10;
+console.log(`ชื่อ = ${tName.toUpperCase()}`);
+console.log(`อายุ = ${age}`);
+//# sourceMappingURL=app.js.map
---
+++ src/app.ts
@@ -0,0 +1,8 @@
+
+let tName:string = "Tjay test";
+let age:number = 30;
+let isWorking:boolean = false;
+age += 10;
+
+console.log(`ชื่อ = ${tName.toUpperCase()}`);
+console.log(`อายุ = ${age}`);
