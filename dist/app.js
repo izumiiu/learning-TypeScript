2bf88a12c9bee1166be23fa1927209273a6419ed
izumiiu <peerawat.ez@gmail.com>
2026-04-28 02:12:03 +0700

    Type Assertions'

--- dist/app.js
+++ dist/app.js
@@ -1,21 +1,10 @@
 "use strict";
-//any $ unknown
 Object.defineProperty(exports, "__esModule", { value: true });
-let fname = "Peerawat";
-fname = 10;
-fname = true;
-function formatNumber(num) {
-    return num.toFixed(2);
-}
-let amount = 50.12345;
-// formatNumber(amount);
-console.log(formatNumber(amount));
-let money = "50.12345";
-money = 50.12345;
-if (typeof money === "number") {
-    console.log(formatNumber(money));
-}
-else {
-    console.log("type of money is not a number");
-}
+let username;
+username = "Peerawat";
+// as string = Type Assertions
+//------แบบแรก---------------
+// console.log((username as string).toUpperCase());
+//------แบบที่สอง-----------------
+console.log(username.toUpperCase());
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,28 +1,10 @@
-//any $ unknown
+let username:unknown;
+username = "Peerawat";
 
-let fname:any = "Peerawat";
-fname = 10;
-fname = true;
-
-function formatNumber(num:number){
-    return num.toFixed(2);
-}
-let amount = 50.12345;
-// formatNumber(amount);
-
-console.log(formatNumber(amount));
-
-let money:unknown = "50.12345";
-money = 50.12345;
-
-if(typeof money === "number"){
-    console.log(formatNumber(money));
-}
-else{
-    console.log("type of money is not a number");
-}
+// as string = Type Assertions
+//------แบบแรก---------------
+// console.log((username as string).toUpperCase());
 
 
-
-
-
+//------แบบที่สอง-----------------
+console.log((<string>username).toUpperCase());
